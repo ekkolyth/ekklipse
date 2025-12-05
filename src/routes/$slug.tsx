9d677a8225225dc8f2e803dc966edafa50d46cef
Mike Kenway <mike@mikekenway.com>
2025-12-04 21:39:22 -0500

    updated metadata

--- src/routes/$slug.tsx
+++ src/routes/$slug.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router';
 import { useQuery } from 'convex/react';
+import { useEffect } from 'react';
 import { api } from '../../convex/_generated/api';
 import { SnippetClient } from '@/components/snippet-client';
 import { LogoHeader } from '@/components/logo-header';
@@ -11,6 +12,24 @@ export const Route = createFileRoute('/$slug')({
 function SnippetPage() {
   const { slug } = Route.useParams();
   const snippet = useQuery(api.snippets.get, { slug });
+
+  useEffect(() => {
+    if (snippet) {
+      document.title = snippet.name;
+      
+      // Update OG title
+      const ogTitle = document.querySelector('meta[property="og:title"]');
+      if (ogTitle) {
+        ogTitle.setAttribute('content', snippet.name);
+      }
+      
+      // Update Twitter title
+      const twitterTitle = document.querySelector('meta[name="twitter:title"]');
+      if (twitterTitle) {
+        twitterTitle.setAttribute('content', snippet.name);
+      }
+    }
+  }, [snippet]);
   
   if (snippet === undefined) {
     return <div className='p-4'>Loading...</div>;
