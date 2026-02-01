ede84023e43ea4794d74f7889045a1973ab357fd
Mike Kenway <mike@mikekenway.com>
2026-01-31 22:26:53 -0500

    added miso scripts folder

---
+++ scripts/deploy/production.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Deploy to Convex production environment
+
+CONVEX_DEPLOY_KEY="prod:clean-iguana-842|eyJ2MiI6IjY4MWNjYjg2ZDg0ODRhZmFhMWRiMmNlZWIxMWUyNDg1In0="
+
+CONVEX_DEPLOY_KEY="$CONVEX_DEPLOY_KEY" pnpm convex deploy
---
+++ scripts/deploy/staging.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Deploy to Convex staging environment
+
+CONVEX_DEPLOY_KEY="dev:valiant-alpaca-954|eyJ2MiI6IjEwMTA2NjMwODg3OTQ5N2Y5NTIwN2VmMmYzMGZhOGE3In0="
+
+CONVEX_DEPLOY_KEY="$CONVEX_DEPLOY_KEY" pnpm convex deploy
